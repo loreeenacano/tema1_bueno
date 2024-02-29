8b7e705015edb4a1f03ca34501e1c50f792020e6
loreeenacano <160004312+loreeenacano@users.noreply.github.com>
2024-02-29 17:17:42 +0000

    c

---
+++ src/main/java/org/example/ejercicio4.java
@@ -0,0 +1,13 @@
+/*Escribe un programa que calcule el mínimo y el máximo de
+una lista de números enteros positivos introducidos por el usuario. La lista
+finalizará cuando se introduzca un número negativo. Realice el ejercicio en
+dos pasos. El primer paso será introducir el número de valores y guardarlos
+en un array. Se recomienda utilizar un array de valor grande y
+posteriormente hacer una copia al valor real. Una vez tenemos los números introducidos en un array,
+se realizan dos funciones independientes. Unacalcula el máximo, otra calcula el mínimo*/
+package org.example;
+
+public class ejercicio4 {
+
+    
+}
