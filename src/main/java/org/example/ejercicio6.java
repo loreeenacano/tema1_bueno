7a81ff8c7e56b033d0d21645ffcd7624b828f5a2
loreeenacano <160004312+loreeenacano@users.noreply.github.com>
2024-03-04 15:08:04 +0000

    cambios

--- src/main/java/org/example/ejercicio6.java
+++ src/main/java/org/example/ejercicio6.java
@@ -1,16 +1,41 @@
 package org.example;
-/*Escribe un programa que muestre por pantalla la lista de los N
-primeros números primos. Realiza una función que reciba N, devuelva un
-array conteniendo los N primeros números primos y posteriormente
-imprima el array en el main o en una función independiente de
-entrada/salida.*/
+import InputOutput.Input;
 
 public class ejercicio6
 {
-    
+    public static int []array_primos(int n)
+    {
+        int numeros_primos_encontrados=0;
+        int divisor=0;
+        int numero=1;
+        int divisores_encontrados=0;
+        int array[]= new int[n];
+
+        while(numeros_primos_encontrados<n)
+        {
+            for(divisor=2; divisor<numero; divisor++)
+            {
+                if(numero%divisor == 0)
+                {
+                    divisores_encontrados++;
+                }
+            }
+            if(divisores_encontrados==0)
+            {
+            System.out.println(numero+" es primo.");
+            numeros_primos_encontrados++;
+            }
+            numero++;
+            divisores_encontrados=0;
+        }
+        array[0]=numero;
+        return array;
+        }
 
     public static void main(String[] args) 
     {
-        
+        System.out.print("Indique la cantidad de numeros primos que quiere recibir:");
+         int n= Input.leerInt();
+         int[] numeros= array_primos(n); 
     }
 }
